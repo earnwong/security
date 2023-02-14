856bd2bf50fd2ce3c3251813c8231000620d9559
earnwong <earn.wonghirundacha@gmail.com>
2023-02-14 15:51:11 -0800

    added comments

--- src/cleandata.py
+++ src/cleandata.py
@@ -10,6 +10,10 @@ class ValidDomain:
         self.valid = False
 
     def is_valid(self):
+        """
+        - checks for valid domain name using regex
+        :return: boolean
+        """
         pattern = r"(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{1,63}(?<!-)\.)+[a-zA-Z]{2,63}$)"
         match = re.match(pattern, self.domain)
         self.valid = match is not None
@@ -21,22 +25,30 @@ class ValidDomain:
 
 class Clean:
     def __init__(self, data):
+        """
+        - takes pandas dataframe and changes it to the desired format, ready for extracting information
+        :param data: pandas dataframe
+        """
         self.df = pd.DataFrame(data, columns=['Host', 'Name', 'Plugin Output'])
         self.df['Name'] = self.df['Name'].str.lower()
         self.df['Plugin Output'] = self.df['Plugin Output'].str.lower()
 
     def get_df(self):
+        """
+        :return: cleaned dataframe
+        """
         return self.df
 
 
 class Search:
     def __init__(self, df):
         self.df = df
-        self.dict_list = {}
+        self.name_dict = {}
 
     def netbios(self):
         """
-        - search for computer names through netbios search
+        - Extract hostnames through netbios search
+        :return: dictionary key(hostname) : value(extracted name)
         """
         # sorts for netbios computer names
         netbios_df = self.df[(self.df['Name'].str.contains("netbios")) & (self.df['Plugin Output'].str.contains("computer name"))]
@@ -48,13 +60,14 @@ class Search:
         computer_names = netbios_df['Plugin Output'].apply(lambda x: re.search(regex, x.replace(" ", "")).group().replace("=computername", ''))
 
         # create a list of tuples from the extracted computer names and the host names
-        self.dict_list.update(dict(zip(netbios_df['Host'], computer_names)))
+        self.name_dict.update(dict(zip(netbios_df['Host'], computer_names)))
 
-        return self.dict_list
+        return self.name_dict
 
     def dns(self):
         """
-        - dns hostname search
+        - Extract hostnames through additional DNS hostname search
+        :return: dictionary key(hostname) : value(extracted name)
         """
         # find dns hostname rows
         dns_df = self.df[self.df['Name'] == "additional dns hostnames"][['Host', 'Plugin Output']]
@@ -63,43 +76,44 @@ class Search:
         dns_df['Plugin Output'] = dns_df['Plugin Output'].str.replace(" ", "").str.replace(
             "thefollowinghostnamespointtotheremotehost:", "").str.replace("\n-", " ").str.split().apply(
             lambda x: x[0] if x else None)  # if hostname already exists then disregard, if doesn't, then add to dict
-        self.dict_list.update(dns_df.set_index('Host').to_dict()['Plugin Output'])
+        self.name_dict.update(dns_df.set_index('Host').to_dict()['Plugin Output'])
 
-        return self.dict_list
+        return self.name_dict
 
     def ssl(self):
         """
-
-        :return: dictionary
+        - Extract hostnames through SSL self-signed certificate search
+        :return: dictionary key(hostname) : value(extracted name)
         """
         df2 = self.df[(self.df['Name'].str.contains("ssl self-signed certificate"))].copy()
 
-        # clean data for only necessary rows
-        for key in self.dict_list:
+        # clean data for only necessary rows, if we have already extracted a name from the original IP address then
+        # disregard the data
+        for key in self.name_dict:
             df2.loc[df2.Host == key, 'Plugin Output'] = np.nan
-
         ssl_df = df2[df2['Plugin Output'].notna()].copy()
 
+        # remove spaces from plugin output data and split data into list
         ssl_df['Plugin Output'] = ssl_df['Plugin Output'].str.replace(" ", "").str.split("[/:]")
 
         def process_row(row):
             """
-
-            :param row:
-            :return:
+            - Extracts the hostname then checks if it is a valid domain name by using ValidDomain class
+            :param row: list - plugin output data
+            :return: the processed row
             """
             s = (row['Plugin Output'])[2::]
             for elem in s:
                 if "cn=" in elem:
                     name = elem.replace("\n", "").replace("cn=", "")
                     valid_domain = ValidDomain(name)
-                    if valid_domain.is_valid() and row["Host"] not in self.dict_list.keys():
-                        self.dict_list[row["Host"]] = name
+                    if valid_domain.is_valid() and row["Host"] not in self.name_dict.keys():
+                        self.name_dict[row["Host"]] = name
             return
 
         ssl_df.apply(process_row, axis=1)
 
-        return self.dict_list
+        return self.name_dict
 
 
 def main():
@@ -107,11 +121,11 @@ def main():
     # data = pd.read_csv(r'/Users/earnsmacbookair/Desktop/General Workstations 20230207.csv')
     data = pd.read_csv(r'/Users/earnsmacbookair/Desktop/academicservers.csv')
     clean = Clean(data)
-    cleaned_df = clean.get_df()
+    cleaned_df = clean.get_df()  # desired dataframe to extract names from
 
     search_data = Search(cleaned_df)
-    dict_list = search_data.netbios()
-    dict_list = search_data.dns()
+    search_data.netbios()
+    search_data.dns()
     dict_list = search_data.ssl()
 
     # make a new column and correlate the host name to the computer names
@@ -119,12 +133,15 @@ def main():
     for key in dict_list:
         cleaned_df.loc[cleaned_df.Host == key, 'Extracted Hostname'] = dict_list[key]
 
-    cleaned_df['Extracted Hostname'].replace('', np.nan, inplace=True)
-    df = cleaned_df[cleaned_df['Extracted Hostname'].notna()]
-    print(df)
+    # code for when we only want to see how many names have been extracted
+    # cleaned_df['Extracted Hostname'].replace('', np.nan, inplace=True)
+    # df = cleaned_df[cleaned_df['Extracted Hostname'].notna()]
+    # os.makedirs('/Users/earnsmacbookair/Desktop/tester', exist_ok=True)
+    # df.to_csv('/Users/earnsmacbookair/Desktop/tester/Only Name.csv')
+    # print(df)
 
     os.makedirs('/Users/earnsmacbookair/Desktop/tester', exist_ok=True)
-    df.to_csv('/Users/earnsmacbookair/Desktop/tester/Output3.csv')
+    cleaned_df.to_csv('/Users/earnsmacbookair/Desktop/tester/Data Output.csv')
 
 
 if __name__ == "__main__":
